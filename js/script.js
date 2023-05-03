297ed6350d09c1ab86679b3717343003a3b5d189
Mohamad-Tanbari <124389317+Mohamad-Tanbari@users.noreply.github.com>
2023-05-03 00:18:32 +0000

    made UI and design

--- js/script.js
+++ js/script.js
@@ -3,3 +3,17 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+/*
+* This function shows how much your loaf order costs
+*/
+function calculate () {
+  // Define variables
+  //const TAX = 0.13
+  const loafType = document.getElementById('loaf-type').value
+  const loafSize = document.getElementById('loaf-size').value
+
+  // output
+  document.getElementById('answer').innerHTML = loafType + '' + loafSize
+  //document.getElementById('answer2').innerHTML = 
+}
